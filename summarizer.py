cdf8670a29c7ccf7d86d72a62cf648809f6444fe
CheralaHarshini <2403a52300@sru.edu.in>
2026-03-11 12:02:44 +0530

    Delete summarizer.py

--- summarizer.py
+++
@@ -1,9 +0,0 @@
-from ai_models import model
-
-def summarize(text):
-
-    prompt=f"Summarize the following text:\n{text}"
-
-    result=model(prompt,max_length=200)
-
-    return result[0]["generated_text"]
